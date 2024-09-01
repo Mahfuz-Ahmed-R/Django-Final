ce942a3d32656bedbc43703e4fb8e92fb0130df4
Mahfuz Ahmed <ahmed.mahfuz.232@northsouth.edu>
2024-09-02 02:09:13 +0600

    commit

--- api/views.py
+++ api/views.py
@@ -153,7 +153,7 @@ class UserRegistrationAPIView(APIView):
             user = serializer.save()
             token = default_token_generator.make_token(user)
             uid = urlsafe_base64_encode(force_bytes(user.pk))
-            confirm_link = f"http://127.0.0.1:8000/active/{uid}/{token}"
+            confirm_link = f"https://django-final-n0lr.onrender.com/active/{uid}/{token}"
             email_subject = "Confirm Email"
             email_body = render_to_string('confirm_email.html', {'confirm_link': confirm_link})
             email = EmailMultiAlternatives(email_subject, email_body, to=[user.email])
