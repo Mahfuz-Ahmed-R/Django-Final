79ab9f085c7ed112fa597fcd8eddd228ea9fd817
Mahfuz Ahmed <ahmed.mahfuz.232@northsouth.edu>
2024-09-07 19:06:14 +0600

    commit

--- payment/views.py
+++ payment/views.py
@@ -49,13 +49,6 @@ class InitiatePayment(APIView):
         print("SSLCOMMERZ Response:", response)
         return Response({'payment_url': response['GatewayPageURL']}, status=status.HTTP_200_OK)
 
-
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.views import APIView
-from django.shortcuts import redirect
-from . import serializers
-
 class PaymentSuccess(APIView):
     def post(self, request, *args, **kwargs):
         # Extract data from request
